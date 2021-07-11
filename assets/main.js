9121fcf3af66cde573eade3e9345aac0265d95e0
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-07-11 14:50:30 -0400

    hamburget menu animation

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,12 @@
 const toggleBtn = document.querySelector(".menu-toggle");
+const menuLines = document.querySelectorAll(".menu-line");
 const projectMainBtn = document.querySelector(".btn-scroll-project");
 const logoBtn = document.querySelector(".btn-scroll-logo");
 const navLinks = document.querySelector(".nav-links");
 const projectSection = document.querySelector(".project-page");
 const landingSection = document.querySelector("#landing-section");
+
+let menuOpen = false;
 
 document.querySelectorAll(".nav-link").forEach((el) => {
   el.addEventListener("click", (e) => {
@@ -21,10 +24,20 @@ logoBtn.addEventListener("click", (e) => {
 toggleBtn.addEventListener("click", (e) => {
   e.preventDefault();
   navLinks.classList.toggle("nav-active");
+  if (!menuOpen) {
+    menuLines.forEach((line, idx) => {
+      line.classList.add(`line-${idx}`);
+      menuOpen = true;
+    });
+  } else {
+    menuLines.forEach((line, idx) => {
+      line.classList.remove(`line-${idx}`);
+      menuOpen = false;
+    });
+  }
 });
 
 projectMainBtn.addEventListener("click", (e) => {
   e.preventDefault();
   projectSection.scrollIntoView({ behavior: "smooth" });
 });
-
